20ac5bc5ce1043f57bfd3ae0e2d0f6f357fae543
Imad <imadetassaoui@gmail.com>
2023-10-31 14:41:09 +0100

    🖼️ Fix product images & product page mobile horizontal scroll

--- components/Products/Element/index.vue
+++ components/Products/Element/index.vue
@@ -5,14 +5,14 @@
     to="/shop/1"
     class="flex flex-col gap-2 w-full md:w-[calc(50%-4px)] lg:w-[calc(25%-6px)]"
   >
-    <div class="w-auto h-[488px] group overflow-hidden relative">
+    <div class="w-auto group overflow-hidden relative">
       <img
-        class="w-full absolute left-0 top-0"
+        class="w-full left-0 top-0"
         src="../../../assets/hoodie2.png"
         alt=""
       />
       <img
-        class="w-full absolute left-0 top-0 group-hover:opacity-0 transition-opacity duration-200 ease-in"
+        class="w-full m-auto top-0 left-0 absolute group-hover:opacity-0 transition-opacity duration-200 ease-in"
         src="../../../assets/hoodie1.png"
         alt=""
       />
