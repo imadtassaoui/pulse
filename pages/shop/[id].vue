6baeec479b64e5d015ecdbb10f45001957095f19
Imad <imadetassaoui@gmail.com>
2023-11-10 16:19:16 +0100

    ✨ Integrated new prod routes

--- pages/shop/[id].vue
+++ pages/shop/[id].vue
@@ -14,6 +14,7 @@ const productId = computed(() => useRoute().params.id);
 console.log(productId.value);
 
 const endpoint = `https://pulse-api-one.vercel.app/${productId.value}`;
+console.log(endpoint);
 
 const { data: product, pending } = await useFetch(endpoint);
 console.log(await product.value);
